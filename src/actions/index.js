af21927e881e46f35edbcbfe237fec77d07ff54d
Arpit Malik <arpit.malik@popxo.com>
2020-03-03 16:48:20 +0530

    http to https

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'f2540ff3524ad68b6a97a6660842f43f';
-const ROOT_URL = 'http://api.openweathermap.org/data/2.5/forecast?appid=' + API_KEY;
+const ROOT_URL = 'https://api.openweathermap.org/data/2.5/forecast?appid=' + API_KEY;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'; //VARIABLE THAT HOLDS ACTION TYPE
 
