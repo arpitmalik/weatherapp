8b89e124f0bf6defcb7496e61c7b4ed575b895d5
Arpit Malik <arpit.malik@popxo.com>
2020-03-04 11:27:36 +0530

    babel

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,20 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_KEY = 'f2540ff3524ad68b6a97a6660842f43f';
-const ROOT_URL = 'https://api.openweathermap.org/data/2.5/forecast?appid=' + API_KEY;
+const API_KEY = "f2540ff3524ad68b6a97a6660842f43f";
+const ROOT_URL =
+  "https://api.openweathermap.org/data/2.5/forecast?appid=" + API_KEY;
 
-export const FETCH_WEATHER = 'FETCH_WEATHER'; //VARIABLE THAT HOLDS ACTION TYPE
+export const FETCH_WEATHER = "FETCH_WEATHER"; //VARIABLE THAT HOLDS ACTION TYPE
 
 //action creator
-export async function fetchWeather (city) {
-    const url = `${ROOT_URL}&q=${city},us`;
-    try {
-        const request = await axios.get(url);    
-        return{
-            type: FETCH_WEATHER,
-            payload: request //returning promise as payload
-        };
-    } catch (error) {
-        alert("This state could not be found.")
-    }
+export async function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  try {
+    const request = await axios.get(url);
+    return {
+      type: FETCH_WEATHER,
+      payload: request //returning promise as payload
+    };
+  } catch (error) {
+    alert("The requested city was not found.");
+  }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,30 @@
 module.exports = {
-  entry: ['./src/index.js'],
+  entry: ["babel-polyfill", "./src/index.js"],
   output: {
     path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
+    publicPath: "/",
+    filename: "bundle.js"
   },
   module: {
     loaders: [
       {
+        test: /\.js?$/,
         exclude: /node_modules/,
-        loader: 'babel',
+        loader: "babel",
         query: {
-          presets: ['react', 'es2015', 'stage-1']
+          presets: ["react", "es2015", "stage-1"]
         }
       }
     ]
   },
+  devtool: "sourcemap",
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ["", ".js", ".jsx"]
   },
   devServer: {
     port: process.env.PORT || 3000,
     historyApiFallback: true,
-    contentBase: './',
+    contentBase: "./",
     watchOptions: {
       aggregateTimeout: 300,
       poll: 1000
